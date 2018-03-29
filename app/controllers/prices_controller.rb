65a8b95ef684f0bf395ded7da8d47c3fff423624
jiikko <n905i.1214@gmail.com>
2018-03-29 23:36:13 +0900

    現在価格と1分間の差を表示する

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -2,6 +2,7 @@ class PricesController < ApplicationController
   def index
     respond_to do |format|
       format.js do
+        @last_trade = BF::Trade.last
         @price_table = BF::Trade.price_table
       end
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def calc_rate(min_diff, max_diff)
+    if min_diff < 0
+      min_diff = 0
+    end
+    if max_diff < 0
+      max_diff = 0
+    end
+    rate = (min_diff / max_diff.to_f)
+    if rate > 1
+      rate = 1
+    end
+    if rate < 0
+      rate = 0
+    end
+    (rate * 100).to_i
+  end
 end
