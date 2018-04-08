078865ef74909b974be04f6bd2b215368b51ea0f
jiikko <n905i.1214@gmail.com>
2018-04-08 20:48:28 +0900

    サーバ起動中にworker数の取得がおかしくなるのを修正する

--- app/controllers/my_trades_controller.rb
+++ app/controllers/my_trades_controller.rb
@@ -18,6 +18,9 @@ class MyTradesController < ApplicationController
   private
 
   def set_my_trade_ships
-    @my_trade_ships = BF::MyTradeShip.limit(5).order(id: :desc)
+    cond = { my_trades: { status: BF::MyTrade.statuses[:requested]} }
+    @my_trade_ships = BF::MyTradeShip.joins(:buy_trade).order(id: :desc).limit(7).where(cond).to_a
+    @my_trade_ships.concat(BF::MyTradeShip.joins(:buy_trade).where.not(cond).order(id: :desc).limit(5))
+    @my_trade_ships.sort_by! { |x| - x.id }
   end
 end
