df02f534c763cd73bacef197213ed37e55e958cd
jiikko <n905i.1214@gmail.com>
2018-04-14 22:00:26 +0900

    クリックしたらworkerのジョブがでてくる

--- app/controllers/resque/workers_controller.rb
+++ app/controllers/resque/workers_controller.rb
@@ -1,5 +1,9 @@
 class Resque::WorkersController < ApplicationController
   def index
     @workers = Resque.workers.select { |x| x.job.present? }
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
