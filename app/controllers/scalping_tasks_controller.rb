c2664064143011990e372d158d4d1a97960a5efd
jiikko <n905i.1214@gmail.com>
2018-05-20 13:01:11 +0900

    tasks一覧を表示する

--- app/controllers/scalping_tasks_controller.rb
+++ app/controllers/scalping_tasks_controller.rb
@@ -1,5 +1,7 @@
 class ScalpingTasksController < ApplicationController
   def index
-    @tasks = BF::ScalpingTask.all
+    @tasks = BF::ScalpingTask.includes(trade_ship: [{ buy_trade: :sell_trade }, :sell_trade]).
+      limit(5).
+      order(created_at: :desc)
   end
 end
