e944df02ff98b7f01b6169c832afea27c7ca6da4
jiikko <n905i.1214@gmail.com>
2018-05-27 00:22:16 +0900

    fix timezone

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,6 +2,6 @@ class ResultsController < ApplicationController
   def index
     @ships_table = BF::MyTradeShip.succeed.includes(:scalping_task, :buy_trade, :sell_trade).
       where("#{BF::MyTradeShip.table_name}.created_at > ?", 2.week.ago).order(created_at: :desc).
-      group_by { |x| x.created_at.strftime('%Y/%m/%d') }
+      group_by { |x| x.created_at.localtime.strftime('%Y/%m/%d') }
   end
 end
