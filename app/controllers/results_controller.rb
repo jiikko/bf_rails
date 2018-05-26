3eb145ce692582ddcf09b987ebdd788a19104a2a
jiikko <n905i.1214@gmail.com>
2018-05-26 19:51:54 +0900

    集計画面を改良する

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,7 +1,7 @@
 class ResultsController < ApplicationController
   def index
-    includes = { trade_ship: [:buy_trade, :sell_trade] }
-    @tasks_table = BF::ScalpingTask.order('created_at desc').includes(includes).
-      where('created_at > ?', 2.week.ago).group_by { |x| x.created_at.strftime('%Y/%m/%d') }
+    @ships_table = BF::MyTradeShip.succeed.includes(:scalping_task, :buy_trade, :sell_trade).
+      where("#{BF::MyTradeShip.table_name}.created_at > ?", 2.week.ago).order(created_at: :desc).
+      group_by { |x| x.created_at.strftime('%Y/%m/%d') }
   end
 end
