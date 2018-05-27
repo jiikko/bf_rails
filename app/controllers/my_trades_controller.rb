df0137e0582793f70da061ae5d08cb0a2d3d8e6d
jiikko <n905i.1214@gmail.com>
2018-05-27 17:39:01 +0900

    tradeページにsourceをだす

--- app/controllers/my_trades_controller.rb
+++ app/controllers/my_trades_controller.rb
@@ -25,8 +25,8 @@ class MyTradesController < ApplicationController
   private
 
   def set_my_trade_ships
-    @my_trade_ships = BF::MyTradeShip.running.includes(:sell_trade, buy_trade: :sell_trade).to_a
-    @my_trade_ships.concat(BF::MyTradeShip.limit(8).order(id: :desc).includes(:sell_trade, buy_trade: :sell_trade))
+    @my_trade_ships = BF::MyTradeShip.running.includes(:scalping_task, :sell_trade, :buy_trade).to_a
+    @my_trade_ships.concat(BF::MyTradeShip.limit(8).order(id: :desc).includes(:scalping_task, :sell_trade, :buy_trade))
     @my_trade_ships.sort_by! { |x| - x.id }
     @my_trade_ships.uniq!
   end
