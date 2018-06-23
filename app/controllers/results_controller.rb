069f21b5661c7a6d5ba1f5aeac103689036d376c
jiikko <n905i.1214@gmail.com>
2018-06-23 11:32:48 +0900

    Fix for ONLY_FULL_GROUP_BY

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,7 +1,7 @@
 class ResultsController < ApplicationController
   def index
     @summarized_my_trades = BF::SummarizedMyTrade.where('summarized_on > ?', 2.week.ago).order(summarized_on: :desc)
-    @total_summarized_my_trade = @summarized_my_trades.group(:kind).
+    @total_summarized_my_trade = @summarized_my_trades.group(:summarized_on, :kind).
       select('sum(count) sum_count, sum(profit) sum_profit, kind').group_by(&:kind)
   end
 end
