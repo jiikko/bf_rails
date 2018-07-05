32fd156e4df6abc4a85996581d50c94780fa662d
jiikko <n905i.1214@gmail.com>
2018-07-06 00:18:54 +0900

    y軸をdrawする

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -9,8 +9,11 @@ class ChartsController < ApplicationController
     # https://stackoverflow.com/questions/9680144/mysql-date-time-round-to-nearest-hour
     trades = BF::Trade.
       where(created_at: day.all_day).
-      select('FROM_UNIXTIME(UNIX_TIMESTAMP(created_at) - MOD(UNIX_TIMESTAMP(created_at),300)) AS time', 'max(price) max_price', 'min(price) min_price').
+      select('FROM_UNIXTIME(UNIX_TIMESTAMP(created_at) - MOD(UNIX_TIMESTAMP(created_at),300)) AS time', 
+             'max(price) max_price',
+             'min(price) min_price').
       group("time")
+    trades = trades.to_a.map { |t| t.time = t.time + 9.hour; t }
     render json: { trades: trades, min_price: min_price, max_price: max_price }
   end
 end
