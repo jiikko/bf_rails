aa083cd093eccf9452405ef5f896f0519396befb
jiikko <n905i.1214@gmail.com>
2018-07-18 21:46:44 +0900

    意図せず乖離率が出ていなかったのを修正する

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,2 @@
 class ApiController < ActionController::Base
-  around_action do
-    # こんなしたいのことがしたい
-    # original_logger = Rails.logger
-    # Rails.logger = Logger.new(File.join(Rails.root, 'log', 'api.log'))
-    # yield
-    # Rails.logger = original_logger
-  end
 end
