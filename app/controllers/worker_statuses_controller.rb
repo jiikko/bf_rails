2ebf323bd389faeae836683fb63bdb7e77322b6d
jiikko <n905i.1214@gmail.com>
2018-08-14 09:09:46 +0900

    キューの数によって色を変える

--- app/controllers/worker_statuses_controller.rb
+++ app/controllers/worker_statuses_controller.rb
@@ -6,6 +6,8 @@ class WorkerStatusesController < ApplicationController
       group_by { |x| x.present? }.
       transform_values { |x| x.size }.
       transform_keys { |x| x ? :working : :idoling }
+    @worker_count = @worker_statuses.values.sum
+    @working_count = @worker_statuses[:working] || 0
 
     respond_to do |format|
       format.js
