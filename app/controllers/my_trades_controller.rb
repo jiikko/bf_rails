423b126abc06684e11eed27b44b0779a9c567ec3
jiikko <n905i.1214@gmail.com>
2018-08-26 14:57:13 +0900

    workerが忙しい時に新しく発注できないようにする

--- app/controllers/my_trades_controller.rb
+++ app/controllers/my_trades_controller.rb
@@ -1,7 +1,12 @@
 class MyTradesController < ApplicationController
   def create
-    BF::MyTrade.new.run_buy_trade!(params[:price])
     @my_trade_ships = BF::MyTradeShip.recent
+      if can_order?
+        BF::MyTrade.new.run_buy_trade!(params[:price])
+        @message = '注文しました'
+      else
+        @message = 'workerの稼働数が最大に近いので注文しませんでした'
+      end
     respond_to do |format|
       format.js
     end
@@ -11,5 +16,12 @@ class MyTradesController < ApplicationController
     my_traded = BF::MyTrade.find(params[:id])
     my_traded.cancel_order!
     head :no_content
+  end
+
+  def can_order?
+    running_worker_count = Resque.workers.select { |worker| worker.job.present? }.count
+    max_count = Resque.workers.count
+    max_orderable = max_count - 1
+    running_worker_count < max_orderable
   end
 end
