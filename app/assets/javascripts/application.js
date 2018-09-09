796f874dc8cc5d91a884aa106abebe1948b54740
jiikko <n905i.1214@gmail.com>
2018-09-09 20:49:34 +0900

    レイアウトを変更する

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,34 @@
 function hide_workers_table(){
   $('#workers').children().remove();
 }
+
+function build_trade_link() {
+  console.log('called build_trade_link')
+  $('[data-role="buy_1"]').toggle('fast')
+  var  min_price = parseInt($('[data-name=min_1_price]').text());
+  $('[data-role="buy_1"]').children().remove()
+  var a_tag = `<a data-method='post' data-remote=true data-method="post" href='/my_trades?price=${min_price}'>${min_price}</a>`
+  $("[data-role='buy_1']").append($(a_tag));
+  $('[data-role="buy_1"]').toggle('fast')
+
+  $('[data-role="buy_5"]').toggle('slow')
+  var min_price = parseInt($('[data-name=min_5_price]').text());
+  $('[data-role="buy_5"]').children().remove()
+  var a_tag = `<a data-method='post' data-remote=true data-method="post" href='/my_trades?price=${min_price}'>${min_price}</a>`
+  $("[data-role='buy_5']").append($(a_tag));
+  $('[data-role="buy_5"]').toggle('fast')
+}
+
+$(document).on('click', '[data-option-column=show]', function() {
+  $('[data-option-column=profit]').toggle();
+  return false;
+})
+
+$(document).on('click', '#worker_button', function() {
+  if($('#workers table').size() >= 1) {
+    hide_workers_table();
+  } else {
+    $.ajax('/resque/workers.js', { type: 'GET', timeout: 1000 });
+  }
+  return false;
+})
