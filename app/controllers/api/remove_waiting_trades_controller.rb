b237844c00baf422af34b7a79b0bdae59bb520ac
jiikko <n905i.1214@gmail.com>
2018-09-19 07:13:10 +0900

    レスポンスにダイアログを使う

--- app/controllers/api/remove_waiting_trades_controller.rb
+++ app/controllers/api/remove_waiting_trades_controller.rb
@@ -1,9 +1,11 @@
 class Api::RemoveWaitingTradesController < ApplicationController
   def create
     BF::RemoveWaitingTradeWorker.perform_async
-    @message = '約定確認を実行します'
     respond_to do |format|
-      format.js { render 'shared/dialog' }
+      format.js do
+        @message = '約定確認を実行します'
+        render 'shared/dialog'
+      end
     end
   end
 end
--- app/controllers/api/scalpings_controller.rb
+++ app/controllers/api/scalpings_controller.rb
@@ -2,7 +2,10 @@ class Api::ScalpingsController < ApplicationController
   def create
     BF::ScalpingWorker.perform_async
     respond_to do |format|
-      format.js { head :ok }
+      format.js do
+        @message = '自動注文を行います'
+        render 'shared/dialog'
+      end
     end
   end
 end
