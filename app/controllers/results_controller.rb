3076dc228dfe83a0c2d5559f8a15091012c69dac
jiikko <n905i.1214@gmail.com>
2018-09-29 23:50:40 +0900

    集計ページで約定トレードがあったら再計算する

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,8 @@
 class ResultsController < ApplicationController
+  CACHE_KEY = 'last_succeed_id'
+
   def index
+    recalc_if_need!
     @summarized_my_trades = BF::SummarizedMyTrade.where('summarized_on > ?', 2.week.ago).order(summarized_on: :desc)
     @total_summarized_my_trade = @summarized_my_trades.
       group(:summarized_on, :kind).
@@ -10,5 +13,14 @@ class ResultsController < ApplicationController
   def recalc
     BF::SummarizedMyTrade.summarize!
     redirect_to results_path, notice: '再計算が完了しました'
+  end
+
+  def recalc_if_need!
+    cached_last_succeed_id = Rails.cache.fetch(CACHE_KEY) { BF::MyTradeShip.succeed.last.id }
+    current_last_succeed_id = BF::MyTradeShip.succeed.last.id
+    unless cached_last_succeed_id == current_last_succeed_id
+      BF::SummarizedMyTrade.summarize!
+      Rails.cache.write(CACHE_KEY, current_last_succeed_id)
+    end
   end
 end
