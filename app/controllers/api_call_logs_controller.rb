c8a16051e2fcb530ea172ef28427ca38383bdd2b
jiikko <n905i.1214@gmail.com>
2018-10-08 00:46:08 +0900

    コール回数が0回だと無になるのを修正する

--- app/controllers/api_call_logs_controller.rb
+++ app/controllers/api_call_logs_controller.rb
@@ -1,8 +1,8 @@
 class ApiCallLogsController < ApplicationController
   def index
     count_table = BF::ApiCallLog.recent.group(:api_type).count
-    @public_api_call_logs_count = count_table["public_api"]
-    @private_api_call_logs_count = count_table["private_api"]
+    @public_api_call_logs_count = count_table["public_api"] || 0
+    @private_api_call_logs_count = count_table["private_api"] || 0
     respond_to do |format|
       format.js
     end
